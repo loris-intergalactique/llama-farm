d92d4cdde44cea964db0dca77c493118c822bdbf
loris-intergalactique <51478929+loris-intergalactique@users.noreply.github.com>
2025-04-27 16:53:36 +0200

    adk tests

---
+++ adk/README.md
@@ -0,0 +1,11 @@
+# Parcours du combattant:
+
+https://google.github.io/adk-docs/get-started/quickstart/#next-steps
+
+Reproducer:
+
+```
+python -m venv .venv
+.venv/bin/pip install google-adk
+.venv/bin/pip install litellm==1.67.2 # Pinned version because of a bug in 1.67.4
+```
---
+++ adk/multi_tool_agent/agent.py
@@ -0,0 +1,73 @@
+import os
+import datetime
+import litellm
+from zoneinfo import ZoneInfo
+from google.adk.agents import LlmAgent
+from google.adk.models.lite_llm import LiteLlm
+litellm._turn_on_debug()
+
+def get_weather(city: str) -> dict:
+    """Retrieves the current weather report for a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the weather report.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+    if city.lower() == "new york":
+        return {
+            "status": "success",
+            "report": (
+                "The weather in New York is sunny with a temperature of 25 degrees"
+                " Celsius (77 degrees Fahrenheit)."
+            ),
+        }
+    else:
+        return {
+            "status": "error",
+            "error_message": f"Weather information for '{city}' is not available.",
+        }
+
+
+def get_current_time(city: str) -> dict:
+    """Returns the current time in a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the current time.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+
+    if city.lower() == "new york":
+        tz_identifier = "America/New_York"
+    else:
+        return {
+            "status": "error",
+            "error_message": (
+                f"Sorry, I don't have timezone information for {city}."
+            ),
+        }
+
+    tz = ZoneInfo(tz_identifier)
+    now = datetime.datetime.now(tz)
+    report = (
+        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
+    )
+    return {"status": "success", "report": report}
+
+os.environ['LM_STUDIO_API_BASE'] = "http://172.26.240.1:1337/api/v0"
+os.environ['LM_STUDIO_API_KEY'] = " # optional, default is empty"
+
+root_agent = LlmAgent(
+    name="weather_time_agent",
+    model=LiteLlm(model="lm_studio/qwen2.5-1.5b-instruct"),
+    description=(
+        "Agent to answer questions about the time and weather in a city."
+    ),
+    instruction=(
+        "You are a helpful agent who can answer user questions about the time and weather in a city."
+    ),
+    tools=[get_weather, get_current_time],
+)
